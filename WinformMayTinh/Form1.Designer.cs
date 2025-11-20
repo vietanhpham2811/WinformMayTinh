941fa40a972419cee6d189b0aaaf3642fd332d69
Phạm Việt Anh <141823836+vietanhpham2811@users.noreply.github.com>
2025-11-20 15:07:38 +0700

    Add project files.

---
+++ WinformMayTinh/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WinformMayTinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnCham = new System.Windows.Forms.Button();
+            this.btxDelete = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnKq = new System.Windows.Forms.Button();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.txtKq = new System.Windows.Forms.TextBox();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(3, 76);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(60, 60);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(69, 76);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(60, 60);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(135, 142);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(60, 60);
+            this.btn6.TabIndex = 3;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(69, 142);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(60, 60);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(3, 142);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(60, 60);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(3, 208);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(60, 60);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(69, 208);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(60, 60);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(135, 208);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(60, 60);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(3, 274);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(126, 60);
+            this.btn0.TabIndex = 9;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnCham
+            // 
+            this.btnCham.Location = new System.Drawing.Point(135, 274);
+            this.btnCham.Name = "btnCham";
+            this.btnCham.Size = new System.Drawing.Size(60, 60);
+            this.btnCham.TabIndex = 10;
+            this.btnCham.Text = ".";
+            this.btnCham.UseVisualStyleBackColor = true;
+            // 
+            // btxDelete
+            // 
+            this.btxDelete.Location = new System.Drawing.Point(201, 76);
+            this.btxDelete.Name = "btxDelete";
+            this.btxDelete.Size = new System.Drawing.Size(129, 60);
+            this.btxDelete.TabIndex = 11;
+            this.btxDelete.Text = "Delete";
+            this.btxDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(201, 142);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(60, 60);
+            this.btnNhan.TabIndex = 12;
+            this.btnNhan.Text = "*";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(270, 142);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(60, 60);
+            this.btnChia.TabIndex = 13;
+            this.btnChia.Text = "/";
+            this.btnChia.UseVisualStyleBackColor = true;
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(270, 208);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(60, 60);
+            this.btnTru.TabIndex = 14;
+            this.btnTru.Text = "-";
+            this.btnTru.UseVisualStyleBackColor = true;
+            // 
+            // btnKq
+            // 
+            this.btnKq.Location = new System.Drawing.Point(270, 274);
+            this.btnKq.Name = "btnKq";
+            this.btnKq.Size = new System.Drawing.Size(60, 60);
+            this.btnKq.TabIndex = 15;
+            this.btnKq.Text = "=";
+            this.btnKq.UseVisualStyleBackColor = true;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(201, 208);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(60, 126);
+            this.btnCong.TabIndex = 16;
+            this.btnCong.Text = "+";
+            this.btnCong.UseVisualStyleBackColor = true;
+            // 
+            // txtKq
+            // 
+            this.txtKq.Location = new System.Drawing.Point(3, 12);
+            this.txtKq.Multiline = true;
+            this.txtKq.Name = "txtKq";
+            this.txtKq.Size = new System.Drawing.Size(327, 49);
+            this.txtKq.TabIndex = 17;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(135, 76);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(60, 60);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(334, 335);
+            this.Controls.Add(this.txtKq);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.btnKq);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btxDelete);
+            this.Controls.Add(this.btnCham);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnCham;
+        private System.Windows.Forms.Button btxDelete;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnKq;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.TextBox txtKq;
+        private System.Windows.Forms.Button btn9;
+    }
+}
+
---
+++ WinformMayTinh/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WinformMayTinh
+{
+    public partial class Form1 : Form
+    {
+        private bool lastWasOperator = false;
+        private bool showingResult = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            WireEvents();
+            InitializeState();
+        }
+
+        private void InitializeState()
+        {
+            if (string.IsNullOrWhiteSpace(txtKq.Text))
+                txtKq.Text = "0";
+            txtKq.ReadOnly = true;
+            AcceptButton = btnKq; // Enter triggers "="
+        }
+
+        private void WireEvents()
+        {
+            // Digits
+            btn0.Click += Digit_Click;
+            btn1.Click += Digit_Click;
+            btn2.Click += Digit_Click;
+            btn3.Click += Digit_Click;
+            btn4.Click += Digit_Click;
+            btn5.Click += Digit_Click;
+            btn6.Click += Digit_Click;
+            btn7.Click += Digit_Click;
+            btn8.Click += Digit_Click;
+            btn9.Click += Digit_Click;
+
+            // Decimal point
+            btnCham.Click += Decimal_Click;
+
+            // Operators
+            btnCong.Click += Operator_Click;
+            btnTru.Click += Operator_Click;
+            btnNhan.Click += Operator_Click;
+            btnChia.Click += Operator_Click;
+
+            // Equals
+            btnKq.Click += Equals_Click;
+
+            // Delete (backspace)
+            btxDelete.Click += Delete_Click;
+
+            // Optional keyboard support
+            KeyPreview = true;
+            KeyPress += Form1_KeyPress;
+        }
+
+        // Digit button handler
+        private void Digit_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn == null) return;
+            var digit = btn.Text;
+
+            if (showingResult)
+            {
+                txtKq.Text = digit;
+                showingResult = false;
+                lastWasOperator = false;
+                return;
+            }
+
+            if (txtKq.Text == "0" && digit != "0")
+            {
+                txtKq.Text = digit;
+            }
+            else
+            {
+                txtKq.Text += digit;
+            }
+
+            lastWasOperator = false;
+        }
+
+        // Decimal point handler
+        private void Decimal_Click(object sender, EventArgs e)
+        {
+            if (showingResult)
+            {
+                txtKq.Text = "0.";
+                showingResult = false;
+                lastWasOperator = false;
+                return;
+            }
+
+            var currentNumber = GetCurrentNumberToken();
+            if (currentNumber.IndexOf(".", StringComparison.Ordinal) >= 0)
+                return; // already has decimal
+
+            if (lastWasOperator || txtKq.Text.Length == 0)
+                txtKq.Text += "0.";
+            else
+                txtKq.Text += ".";
+
+            lastWasOperator = false;
+        }
+
+        // Operator handler
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn == null) return;
+            string op = btn.Text; // + - * /
+
+            if (showingResult)
+            {
+                showingResult = false;
+            }
+
+            if (txtKq.Text.Length == 0)
+            {
+                if (op == "-")
+                {
+                    txtKq.Text = "-";
+                    lastWasOperator = false;
+                }
+                return;
+            }
+
+            char lastChar = txtKq.Text[txtKq.Text.Length - 1];
+            if (IsOperator(lastChar))
+            {
+                // replace previous operator
+                txtKq.Text = txtKq.Text.Substring(0, txtKq.Text.Length - 1) + op;
+            }
+            else
+            {
+                txtKq.Text += op;
+            }
+
+            lastWasOperator = true;
+        }
+
+        // Equals handler
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expr = txtKq.Text;
+                if (string.IsNullOrWhiteSpace(expr)) return;
+
+                // Remove trailing operator(s)
+                while (expr.Length > 0 && IsOperator(expr[expr.Length - 1]))
+                    expr = expr.Substring(0, expr.Length - 1);
+
+                if (expr.Length == 0) return;
+
+                object raw = new DataTable().Compute(expr, null);
+                string result = FormatResult(raw);
+                txtKq.Text = result;
+                showingResult = true;
+                lastWasOperator = false;
+            }
+            catch
+            {
+                txtKq.Text = "Error";
+                showingResult = true;
+                lastWasOperator = false;
+            }
+        }
+
+        // Delete (backspace) handler
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (showingResult)
+            {
+                txtKq.Text = "0";
+                showingResult = false;
+                lastWasOperator = false;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtKq.Text) || txtKq.Text == "0" || txtKq.Text == "Error")
+            {
+                txtKq.Text = "0";
+                lastWasOperator = false;
+                return;
+            }
+
+            txtKq.Text = txtKq.Text.Substring(0, txtKq.Text.Length - 1);
+            if (txtKq.Text.Length == 0)
+            {
+                txtKq.Text = "0";
+                lastWasOperator = false;
+                return;
+            }
+
+            lastWasOperator = IsOperator(txtKq.Text[txtKq.Text.Length - 1]);
+        }
+
+        // Keyboard support
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+
+            if (char.IsDigit(c))
+            {
+                Digit_Click(new Button { Text = c.ToString() }, EventArgs.Empty);
+                e.Handled = true;
+            }
+            else if (c == '.' || c == ',')
+            {
+                Decimal_Click(btnCham, EventArgs.Empty);
+                e.Handled = true;
+            }
+            else if ("+-*/".IndexOf(c) >= 0)
+            {
+                Operator_Click(new Button { Text = c.ToString() }, EventArgs.Empty);
+                e.Handled = true;
+            }
+            else if (c == '=' || c == '\r')
+            {
+                Equals_Click(btnKq, EventArgs.Empty);
+                e.Handled = true;
+            }
+            else if (c == '\b')
+            {
+                Delete_Click(btxDelete, EventArgs.Empty);
+                e.Handled = true;
+            }
+        }
+
+        // Helper: is operator
+        private static bool IsOperator(char ch)
+        {
+            return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+        }
+
+        // Helper: get current number token after last operator
+        private string GetCurrentNumberToken()
+        {
+            string s = txtKq.Text;
+            if (string.IsNullOrEmpty(s)) return "";
+            int i = s.Length - 1;
+            while (i >= 0 && !IsOperator(s[i]))
+                i--;
+            return s.Substring(i + 1);
+        }
+
+        // Helper: format result removing trailing .0
+        private string FormatResult(object raw)
+        {
+            if (raw == null) return "0";
+            double d;
+            if (double.TryParse(Convert.ToString(raw), out d))
+            {
+                if (Math.Abs(d % 1) < 1e-12)
+                    return ((long)Math.Round(d)).ToString();
+                return d.ToString();
+            }
+            return raw.ToString();
+        }
+
+    
+    }
+}
